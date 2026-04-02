3665bfa8f47266e926cf3d282c8474b95340d90e
Sparsh Kashyap <sparshkashyap655@gmail.com>
2026-04-02 13:12:02 +0530

    feat: replace temp ChatbotButton implementation with a new version

--- frontend/src/components/chatbot/ChatbotLauncher.tsx
+++ frontend/src/components/chatbot/ChatbotLauncher.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import ChatbotButton from "./temp";
+import ChatbotButton from "./ChatbotButton_temp";
 import ChatbotWindow from "./ChatbotWindow";
 
 const hiddenPaths = ["/login", "/signup", "/forgot-password"];
